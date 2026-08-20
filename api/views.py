f2a6c5681c8ec6f7a0a8ae6e5655b463f3626f49
Anatoly Pavlinin <anatolianych@gmail.com>
2026-08-20 11:26:47 +0300

    views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import RegisterSerializer
+from .models import CustomUser
+from rest_framework.authtoken.models import Token
 
-# Create your views here.
+
+class RegisterView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class LoginView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        from django.contrib.auth import authenticate
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+        user = authenticate(request, email=email, password=password)
+
+        if user is not None:
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key})
+        else:
+            return Response({'error': 'Неверные учетные данные'}, status=400)
